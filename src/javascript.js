3735cbb7a00c7229b75af222d6971ccfa73b9151
SandraBexx <117389232+SandraBexx@users.noreply.github.com>
2023-01-07 12:52:22 +0100

    Axios und API installed

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,11 @@
+function displayData(response) {
+  console.log(response.data.main.temp);
+}
 
+//let apiKey = "3ea3b1bcf6tod7e43390a734fd5a0186";
+//let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=&${apiKey}&units=metric`;
+
+let apiKey = "be79f322990c75a1ac80877977e8bf4e";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayData);
