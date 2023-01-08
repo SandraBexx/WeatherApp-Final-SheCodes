2a6da86e0db554d62cfcbd68845d4e6916ab0255
SandraBexx <117389232+SandraBexx@users.noreply.github.com>
2023-01-08 07:55:14 +0100

    add wind speed in html + style

--- src/javascript.js
+++ src/javascript.js
@@ -1,16 +1,11 @@
-function displayData(response) {
+function displayTemperature(response) {
   console.log(response);
-  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#currentTemperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
-
-//let apiKey = "3ea3b1bcf6tod7e43390a734fd5a0186";
-//let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=&${apiKey}&units=metric`;
-
-//let apiKey = "be79f322990c75a1ac80877977e8bf4e";
-//let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=${apiKey}&units=metric`;
 
 let city = "Lisbon";
 let apiKey = "3ea3b1bcf6tod7e43390a734fd5a0186";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayData);
+axios.get(apiUrl).then(displayTemperature);
