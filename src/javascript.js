88a829d88fb80b87607a426269b64d0f4e9f2a16
SandraBexx <117389232+SandraBexx@users.noreply.github.com>
2023-01-08 08:35:14 +0100

    fetch weather data and add it in html

--- src/javascript.js
+++ src/javascript.js
@@ -1,11 +1,19 @@
-function displayTemperature(response) {
+function displayWeatherData(response) {
   console.log(response);
+  let cityElement = document.querySelector("#searchedCity");
+  cityElement.innerHTML = response.data.city;
   let temperatureElement = document.querySelector("#currentTemperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let rainElement = document.querySelector("#rainPossibility");
+  rainElement.innerHTML = Math.round(response.data.temperature.humidity);
+  let windElement = document.querySelector("#windSpeed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let city = "Lisbon";
 let apiKey = "3ea3b1bcf6tod7e43390a734fd5a0186";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayWeatherData);
